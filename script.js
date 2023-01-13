cc38a3788cafe46ca9a726010e60e72de2ae97d9
pooja062000 <vikaskosli9890@gmail.com>
2023-01-13 18:45:58 +0530

    changing page

--- script.js
+++ script.js
@@ -1,37 +1,40 @@
 function getTime(){
 
-    let timesNow = new Date()
+    let currentTime = new Date()
+    let hours = currentTime.getHours()
+    let minutes = currentTime.getMinutes()
+    let seconds = currentTime.getSeconds()
 
-    let hrs = timesNow.getHours()
-    let mints = timesNow.getMinutes()
-    let seds = timesNow.getSeconds()
-    
 
-    
-    if(hrs > 12){
-        hrs = hrs - 12
-        document.querySelector('#sess').innerText = 'PM'
+    if(hours > 12){
+        hours = hours - 12
+        document.querySelector('#session').innerText = 'PM'
     }else{
-        document.querySelector('#sess').innerText = 'AM'
+        document.querySelector('#session').innerText = 'AM'
     }
 
-
-    
-
-    if(hr < 10){
-        hrs = '0' + hrs
-    } else if (mints < 10){
-        mints = '0' + mints
-    } else if (seds < 10){
-        seds = '0' + seds
+    if(hours <12){
+        document.querySelector('.greet').innerText = 'Good Night'
+    }else{
+        document.querySelector('.greet').innerText = 'Good Morning'
     }
 
-    document.querySelector('#hr').innerText = hrs;
-    document.querySelector('#mint').innerText = mints;
-    document.querySelector('#sed').innerText = seds;
-  
-  
+ 
     
+    if(hours < 10){
+        hours = '0' + hours
+    } else if (minutes < 10){
+        minutes = '0' + minutes
+    } else if (seconds < 10){
+        seconds = '0' + seconds
+    }else{
+
+    }
+
+    document.querySelector('#hour').innerText = hours;
+    document.querySelector('#minute').innerText = minutes;
+    document.querySelector('#second').innerText = seconds;
+
 }
 
 setInterval(getTime, 1000)
